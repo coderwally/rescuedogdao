79952fbb88823de15ca394584aa7ef003a292742
Coder Wally <97439397+coderwally@users.noreply.github.com>
2022-11-27 16:30:55 +0100

    dapp updates

--- dapp/src/App.jsx
+++ dapp/src/App.jsx
@@ -1,9 +1,18 @@
-import { useAddress, ConnectWallet } from '@thirdweb-dev/react';
+import { useAddress, ConnectWallet, useContract, useNFTBalance, Web3Button } from '@thirdweb-dev/react';
+import { useState, useEffect, useMemo } from 'react';
 
 const App = () => {
   const address = useAddress();
-  console.log("👋 Address:", address);
 
+  const editionDropAddress = "0x2E7acDD428f4ff041aEe739179552769390Ff387"
+  const { contract: editionDrop } = useContract(editionDropAddress, "edition-drop");
+  const { data: nftBalance } = useNFTBalance(editionDrop, address, "0")
+
+  const hasClaimedNFT = useMemo(() => {
+    return nftBalance && nftBalance.gt(0)
+  }, [nftBalance])
+
+  //Wallet not connected
   if (!address) {
     return (
       <div className="landing">
@@ -15,10 +24,36 @@ const App = () => {
     );
   }
 
+  if (hasClaimedNFT) {
+    return (
+      <div className="member-page">
+        <h1>🐶 DAO Member Page</h1>
+        <p>Congratulations on being a member</p>
+      </div>
+    );
+  };
+
   return (
-    <div className="landing">
-      <h1>🐶 wallet connected, now what!</h1>
-    </div>);
+<div className="mint-nft">
+      <h1>Rescue Dog DAO<br />Membership NFT 🐶</h1>
+      <div className="btn-hero">
+        <Web3Button 
+          contractAddress={editionDropAddress}
+          action={contract => {
+            contract.erc1155.claim(0, 1)
+          }}
+          onSuccess={() => {
+            console.log(`🌊 Successfully Minted! Check it out on OpenSea: https://testnets.opensea.io/assets/${editionDrop.getAddress()}/0`);
+          }}
+          onError={error => {
+            console.error("Failed to mint NFT", error);
+          }}
+        >
+          Mint your NFT<br/>(0.0001 $Goerli-ETH)
+        </Web3Button>
+      </div>
+    </div>    
+    );
 }
 
 export default App;
